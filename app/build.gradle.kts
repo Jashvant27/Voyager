a738d5cbc8ae2d21d7e3947521f7440893c0cf91
Jashvant Sewmangal <jashy@hotmail.nl>
2025-10-09 17:38:44 +0200

    Fixed annotations

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,9 +2,9 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.hilt)
     alias(libs.plugins.kotlin.parcelize)
     alias(libs.plugins.kotlin.ksp)
+    alias(libs.plugins.hilt)
 }
 
 android {
